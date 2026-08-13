89bb08514f1a1e54c1af70e4ca153d9aea7e80c7
Katrin3536 <dudko.katerina86gmail.com>
2026-08-13 12:47:47 +0300

    add all files

---
+++ src/s2-homeworks/hw08/User.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { UserType } from './HW8'
+import s from './HW8.module.css'
+
+// types
+type UserPropsType = {
+    u: UserType
+}
+
+const User: React.FC<UserPropsType> = ({ u }:UserPropsType) => {
+    return (
+        <tr id={'hw8-user-' + u._id + '-' + u.age} className={s.item}>
+            <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
+                <span>{u.name}</span>
+
+            </td>
+            <td id={'hw8-user-age-' + u._id}>
+                <span>{u.age}</span>
+
+            </td>
+        </tr>
+    )
+}
+
+export default User
